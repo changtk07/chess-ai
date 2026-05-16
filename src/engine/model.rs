10aa6dddf50b883c795d4aba5e07042425823987
Kevin Chang <zhangdaogang07@gmail.com>
2026-05-17 04:31:04 +0800

    refactor: update model architecture with attention-based value head and board-state-aware policy head

--- src/engine/model.rs
+++ src/engine/model.rs
@@ -1,6 +1,6 @@
 use burn::{
     config::Config,
-    module::Module,
+    module::{Module, Param},
     nn::{
         attention::{MhaInput, MultiHeadAttention, MultiHeadAttentionConfig},
         Linear, LinearConfig, RmsNorm, RmsNormConfig,
@@ -8,6 +8,7 @@ use burn::{
     tensor::{
         activation::{gelu, softmax, tanh},
         backend::Backend,
+        Distribution,
     },
     Tensor,
 };
@@ -22,7 +23,7 @@ struct AttentionBlock<B: Backend> {
 }
 
 impl<B: Backend> AttentionBlock<B> {
-    // [batch_size, seq_len, d_model]
+    // [batch_size, 65, d_model]
     fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 3> {
         let x = self.norm1.forward(input.clone());
         let x = self.attention.forward(MhaInput::self_attn(x)).context;
@@ -59,16 +60,17 @@ impl AttentionBlockConfig {
 
 #[derive(Module, Debug)]
 struct PolicyHead<B: Backend> {
-    ff1: Linear<B>,
-    ff2: Linear<B>,
+    ff: Linear<B>,
 }
 
 impl<B: Backend> PolicyHead<B> {
-    // [batch_size, d_model]
-    fn forward(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
-        let x = self.ff1.forward(input);
-        let x = gelu(x);
-        let x = self.ff2.forward(x);
+    pub(crate) const N_MOVE_PLANES: usize = 73;
+
+    // [batch_size, 64, d_model]
+    fn forward(&self, input: Tensor<B, 3>, legal_mask: Tensor<B, 2>) -> Tensor<B, 2> {
+        let x = self.ff.forward(input);
+        let x = x.flatten(1, 2);
+        let x = x + legal_mask;
         softmax(x, 1)
     }
 }
@@ -77,31 +79,40 @@ impl<B: Backend> PolicyHead<B> {
 struct PolicyHeadConfig {
     #[config(default = 384)]
     d_model: usize,
-    #[config(default = 384)]
-    d_ff: usize,
 }
 
 impl PolicyHeadConfig {
-    const D_OUT: usize = 4672;
-
     fn init<B: Backend>(&self, device: &B::Device) -> PolicyHead<B> {
         PolicyHead {
-            ff1: LinearConfig::new(self.d_model, self.d_ff).init(device),
-            ff2: LinearConfig::new(self.d_ff, Self::D_OUT).init(device),
+            ff: LinearConfig::new(self.d_model, PolicyHead::<B>::N_MOVE_PLANES).init(device),
         }
     }
 }
 
 #[derive(Module, Debug)]
 struct ValueHead<B: Backend> {
+    query: Param<Tensor<B, 3>>,
+    keys: Linear<B>,
+    values: Linear<B>,
     ff1: Linear<B>,
     ff2: Linear<B>,
 }
 
 impl<B: Backend> ValueHead<B> {
-    // [batch_size, d_model]
-    fn forward(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
-        let x = self.ff1.forward(input);
+    // [batch_size, 64, d_model]
+    fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 2> {
+        let batch_size = input.shape().dims[0] as i64;
+        let d_model = input.shape().dims[2] as f64;
+
+        let query = self.query.val().expand([batch_size, -1, -1]);
+        let keys = self.keys.forward(input.clone()).swap_dims(1, 2);
+        let values = self.values.forward(input);
+
+        let scores = query.matmul(keys) / d_model.sqrt();
+        let weights = softmax(scores, 2);
+
+        let x = weights.matmul(values).squeeze_dim(1);
+        let x = self.ff1.forward(x);
         let x = gelu(x);
         let x = self.ff2.forward(x);
         tanh(x)
@@ -112,17 +123,20 @@ impl<B: Backend> ValueHead<B> {
 struct ValueHeadConfig {
     #[config(default = 384)]
     d_model: usize,
-    #[config(default = 384)]
-    d_ff: usize,
 }
 
 impl ValueHeadConfig {
-    const D_OUT: usize = 1;
-
     fn init<B: Backend>(&self, device: &B::Device) -> ValueHead<B> {
         ValueHead {
-            ff1: LinearConfig::new(self.d_model, self.d_ff).init(device),
-            ff2: LinearConfig::new(self.d_ff, Self::D_OUT).init(device),
+            query: Param::from_tensor(Tensor::random(
+                [1, 1, self.d_model],
+                Distribution::Normal(0.0, (1.0 / self.d_model as f64).sqrt()),
+                device,
+            )),
+            keys: LinearConfig::new(self.d_model, self.d_model).init(device),
+            values: LinearConfig::new(self.d_model, self.d_model).init(device),
+            ff1: LinearConfig::new(self.d_model, self.d_model).init(device),
+            ff2: LinearConfig::new(self.d_model, 1).init(device),
         }
     }
 }
@@ -133,6 +147,12 @@ pub(crate) struct TransformerModel<B: Backend> {
     attention_blocks: Vec<AttentionBlock<B>>,
     policy_head: PolicyHead<B>,
     value_head: ValueHead<B>,
+}
+
+impl<B: Backend> TransformerModel<B> {
+    pub(crate) const HEAD_DIMENSION: usize = 64;
+
+    // TODO: forward
 }
 
 #[derive(Config, Debug)]
@@ -146,10 +166,8 @@ pub(crate) struct TransformerModelConfig {
 }
 
 impl TransformerModelConfig {
-    const HEAD_DIMENSION: usize = 64;
-
     pub(crate) fn init<B: Backend>(&self, device: &B::Device) -> TransformerModel<B> {
-        let d_model = self.n_heads * Self::HEAD_DIMENSION;
+        let d_model = self.n_heads * TransformerModel::<B>::HEAD_DIMENSION;
         let d_ff = (self.d_ff_scale * d_model as f64) as usize;
 
         TransformerModel {
@@ -162,14 +180,8 @@ impl TransformerModelConfig {
                         .init(device)
                 })
                 .collect(),
-            policy_head: PolicyHeadConfig::new()
-                .with_d_model(d_model)
-                .with_d_ff(d_model)
-                .init(device),
-            value_head: ValueHeadConfig::new()
-                .with_d_model(d_model)
-                .with_d_ff(d_model)
-                .init(device),
+            policy_head: PolicyHeadConfig::new().with_d_model(d_model).init(device),
+            value_head: ValueHeadConfig::new().with_d_model(d_model).init(device),
         }
     }
 }
